c3adcdf96fab1d170916119e903ac36f7c93caa6
drkchiloll <samwomack_@me.com>
2016-02-05 14:22:16 -0600

    default parameters in functions using (something='def msg') =>

--- js/es6.js
+++ js/es6.js
@@ -1,17 +1,10 @@
 'use strict';
 
-(function() {
-  console.log(block);
-
-  if(true) {
-    // Create Variable on Associated to it Container BLOCK
-    // NOT HOISTED..Anything Above is the Temporal Dead Zone
-    console.log(block);
-    let block = 'block';
-
-    const PI
-
-    PI = 3.14159
-    console.log(PI);
+(() => {
+  var logSomething = (something = 'default message') => {
+    console.log(something);
   }
-}());
+
+  logSomething();
+  logSomething('not default');
+})()

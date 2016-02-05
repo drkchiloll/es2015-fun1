2162c8d53ebbb3b0bd7e7ebeea10d21e98f3dd07
drkchiloll <samwomack_@me.com>
2016-02-05 14:35:45 -0600

    demo'd various uses of the ...spread operator

--- js/es6.js
+++ js/es6.js
@@ -7,4 +7,20 @@
 
   logSomething();
   logSomething(undefined, 'and overriding');
+
+  var sum = (a, b, c) => a + b + c;
+  let nums = [1, 2, 3];
+  // Use of Spread Operator
+  console.log(sum(...nums)); // 6
+
+  // Spread Operator in Array Literals
+  let arr = [...nums, 4, 5, 6];
+  console.log(arr); // [1, 2, 3, 4, 5, 6]
+
+  // Spread Operator in FN Parameter (REST Param)
+  var joinStuff = (...stuffs) => {
+    console.log(stuffs.join(' '));
+  }
+  // Any Number of ARGs will Work
+  joinStuff('Stuffs', 'to', 'join');
 })()

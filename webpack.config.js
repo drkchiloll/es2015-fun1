323cacf253797c5a94784573494ea7177fb3dd99
drkchiloll <samwomack_@me.com>
2016-02-06 10:10:05 -0600

    converted project to use webpack module bundling beside grunt

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'js'),
+  build: path.join(__dirname, 'dist')
+};
+
+module.exports = {
+  entry: PATHS.app + '/es6.js',
+  resolve: {
+    extensions: ['', '.js']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'es5.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: PATHS.app
+    }]
+  }
+};

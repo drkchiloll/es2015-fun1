72d699f693fa2ee8bb997b1de4f468c230f92be4
drkchiloll <samwomack_@me.com>
2016-02-07 11:48:42 -0600

    demo of how Sets work in es2015

--- js/es6.js
+++ js/es6.js
@@ -1,51 +1,25 @@
 'use strict';
 
 (() => {
-  var logSomething = (something = 'default message', somethingElse = `${something} with concatenation`) => {
-    console.log(something, somethingElse);
-  }
+  var mySet = new Set();
 
-  logSomething();
-  logSomething(undefined, 'and overriding');
+  let anArray = [1,1,1,2,3,4,4,5];
+  let myOtherSet = new Set(anArray);
 
-  var sum = (a, b, c) => a + b + c;
-  let nums = [1, 2, 3];
-  // Use of Spread Operator
-  console.log(sum(...nums)); // 6
+  console.log(myOtherSet); //Removes all Duplicates
+  console.log(myOtherSet.size);
 
-  // Spread Operator in Array Literals
-  let arr = [...nums, 4, 5, 6];
-  console.log(arr); // [1, 2, 3, 4, 5, 6]
+  mySet.add('something').add('else');
+  console.log(mySet);
+  mySet.delete('else');
+  console.log(mySet, `delete('else') removed else`);
+  mySet.clear();
+  console.log(mySet, ' was cleared()');
+  console.log(mySet.has('something'), 'has(\'value\')');
 
-  // Spread Operator in FN Parameter (REST Param)
-  var joinStuff = (...stuffs) => {
-    console.log(stuffs.join(' '));
-  }
-  // Any Number of ARGs will Work
-  joinStuff('Stuffs', 'to', 'join');
-  console.log();
-  console.log('Template Strings');
-  // Template Strings
-  // Line Breaks are Legel
-  var template = `this is a template
-string!`;
-  console.log(template);
+  myOtherSet.forEach((value1, value2, set) => {
+    console.log(value1);
+  });
 
-  // Interpolation using ${string} Tokens
-  let a = '5',
-      b = 'something';
-  console.log(`a is ${a}, while b is ${b} else`);
-
-  var reverseString = (string) => string.split('').reverse().join('');
-  console.log(`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
-
-  // Tagged Template Strings
-  var str = (literals, ...values) => {
-    // console.log(literals);
-    // console.log(values);
-    return values[2] + literals[3] + literals[2] + values[1] + literals[1] + values[0] + literals[0];
-  };
-
-  console.log(str`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
-
+  console.log(myOtherSet[0]); //undefined
 })()

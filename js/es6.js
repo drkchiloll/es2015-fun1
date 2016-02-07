54a99232d4476a19f59c6cff0d782f0657753f1e
drkchiloll <samwomack_@me.com>
2016-02-07 11:54:03 -0600

    Maps demo for es2015

--- js/es6.js
+++ js/es6.js
@@ -1,51 +1,14 @@
 'use strict';
 
 (() => {
-  var logSomething = (something = 'default message', somethingElse = `${something} with concatenation`) => {
-    console.log(something, somethingElse);
-  }
+  var myMap = new Map();
 
-  logSomething();
-  logSomething(undefined, 'and overriding');
+  let myOtherMap = new Map([['a', 'b'], [1, 1]]);
+  console.log(myOtherMap);
 
-  var sum = (a, b, c) => a + b + c;
-  let nums = [1, 2, 3];
-  // Use of Spread Operator
-  console.log(sum(...nums)); // 6
+  myMap.set('test', {e: 'f'});
+  console.log(myMap.get('test'));
+  console.log(myMap.has('oops'));
 
-  // Spread Operator in Array Literals
-  let arr = [...nums, 4, 5, 6];
-  console.log(arr); // [1, 2, 3, 4, 5, 6]
-
-  // Spread Operator in FN Parameter (REST Param)
-  var joinStuff = (...stuffs) => {
-    console.log(stuffs.join(' '));
-  }
-  // Any Number of ARGs will Work
-  joinStuff('Stuffs', 'to', 'join');
-  console.log();
-  console.log('Template Strings');
-  // Template Strings
-  // Line Breaks are Legel
-  var template = `this is a template
-string!`;
-  console.log(template);
-
-  // Interpolation using ${string} Tokens
-  let a = '5',
-      b = 'something';
-  console.log(`a is ${a}, while b is ${b} else`);
-
-  var reverseString = (string) => string.split('').reverse().join('');
-  console.log(`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
-
-  // Tagged Template Strings
-  var str = (literals, ...values) => {
-    // console.log(literals);
-    // console.log(values);
-    return values[2] + literals[3] + literals[2] + values[1] + literals[1] + values[0] + literals[0];
-  };
-
-  console.log(str`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
-
+  console.log(myOtherMap.keys());
 })()

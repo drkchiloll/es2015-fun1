30104d3b4fc1d882a621f1b7eb0c3053c1e89a18
drkchiloll <samwomack_@me.com>
2016-02-07 12:04:52 -0600

    Iterator demo

--- js/es6.js
+++ js/es6.js
@@ -1,51 +1,23 @@
 'use strict';
 
 (() => {
-  var logSomething = (something = 'default message', somethingElse = `${something} with concatenation`) => {
-    console.log(something, somethingElse);
-  }
 
-  logSomething();
-  logSomething(undefined, 'and overriding');
+  var mySet = new Set(['a', 'b', 'c']),
+      items = mySet.entries();
+  console.log(items.next());
 
-  var sum = (a, b, c) => a + b + c;
-  let nums = [1, 2, 3];
-  // Use of Spread Operator
-  console.log(sum(...nums)); // 6
+  let myMap = new Map([['a', 1], ['b', 2]]),
+      myMapItems = myMap.entries();
 
-  // Spread Operator in Array Literals
-  let arr = [...nums, 4, 5, 6];
-  console.log(arr); // [1, 2, 3, 4, 5, 6]
+  console.log(myMapItems.next());
+  console.log(myMapItems.next());
+  console.log(myMapItems.next()); // value=undefined done=true
 
-  // Spread Operator in FN Parameter (REST Param)
-  var joinStuff = (...stuffs) => {
-    console.log(stuffs.join(' '));
-  }
-  // Any Number of ARGs will Work
-  joinStuff('Stuffs', 'to', 'join');
-  console.log();
-  console.log('Template Strings');
-  // Template Strings
-  // Line Breaks are Legel
-  var template = `this is a template
-string!`;
-  console.log(template);
+  let setValues = mySet.values(),
+      mapValues = myMap.values();
+  console.log(setValues.next());
+  console.log(mapValues.next());
 
-  // Interpolation using ${string} Tokens
-  let a = '5',
-      b = 'something';
-  console.log(`a is ${a}, while b is ${b} else`);
-
-  var reverseString = (string) => string.split('').reverse().join('');
-  console.log(`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
-
-  // Tagged Template Strings
-  var str = (literals, ...values) => {
-    // console.log(literals);
-    // console.log(values);
-    return values[2] + literals[3] + literals[2] + values[1] + literals[1] + values[0] + literals[0];
-  };
-
-  console.log(str`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
-
+  let mapKeys = myMap.keys();
+  console.log(mapKeys);
 })()

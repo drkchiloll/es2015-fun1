90a35dd9355288ed62c74d0407a76c4592d2e369
drkchiloll <samwomack_@me.com>
2016-02-07 08:32:02 -0600

    changes with Object Literals..syntatic..

--- js/es6.js
+++ js/es6.js
@@ -1,51 +1,29 @@
 'use strict';
 
 (() => {
-  var logSomething = (something = 'default message', somethingElse = `${something} with concatenation`) => {
-    console.log(something, somethingElse);
-  }
 
-  logSomething();
-  logSomething(undefined, 'and overriding');
+  var x = 'test',
+      y = 42;
 
-  var sum = (a, b, c) => a + b + c;
-  let nums = [1, 2, 3];
-  // Use of Spread Operator
-  console.log(sum(...nums)); // 6
-
-  // Spread Operator in Array Literals
-  let arr = [...nums, 4, 5, 6];
-  console.log(arr); // [1, 2, 3, 4, 5, 6]
-
-  // Spread Operator in FN Parameter (REST Param)
-  var joinStuff = (...stuffs) => {
-    console.log(stuffs.join(' '));
-  }
-  // Any Number of ARGs will Work
-  joinStuff('Stuffs', 'to', 'join');
-  console.log();
-  console.log('Template Strings');
-  // Template Strings
-  // Line Breaks are Legel
-  var template = `this is a template
-string!`;
-  console.log(template);
-
-  // Interpolation using ${string} Tokens
-  let a = '5',
-      b = 'something';
-  console.log(`a is ${a}, while b is ${b} else`);
-
-  var reverseString = (string) => string.split('').reverse().join('');
-  console.log(`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
-
-  // Tagged Template Strings
-  var str = (literals, ...values) => {
-    // console.log(literals);
-    // console.log(values);
-    return values[2] + literals[3] + literals[2] + values[1] + literals[1] + values[0] + literals[0];
+  let myObj = {
+    x, y
   };
+  console.log(myObj.x);
+  console.log(myObj.y);
 
-  console.log(str`a is ${a}, while b is ${b} else, and ${reverseString(b)} is reversed`);
+  let objectWithMethod = {
+    someMethod(aParam) {
+      console.log(aParam);
+    }
+  };
+  objectWithMethod.someMethod('yay es6');
 
+  let dynamicPropertyName = {
+    [x+y] : 'some value'
+  };
+  console.log(dynamicPropertyName.test42);
+
+  // Target Object, SRC Objects Copied From
+  let composedObj = Object.assign({}, myObj, objectWithMethod, dynamicPropertyName);
+  console.log(composedObj);
 })()
